f9e01de0bf9c23aa062afccefd522988de92b35b
Rob Friesel <robf@dealer.com>
2011-08-05 14:13:05 -0400

    initial version of Punch-It-Up

---
+++ src/js/punch-it-up.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Generator at http://benalman.com/code/test/jquery-run-code-bookmarklet/
+ * 
+ * TODO - configurable options etc.
+ */
+
+var r = function(n){
+	return Math.floor( n || 10 * Math.random() );
+};
+
+jQuery('html *').css({
+	border: r() + 'px solid red',
+	'borderRadius': r() + 'px', 'padding': r() + 'px'
+});
